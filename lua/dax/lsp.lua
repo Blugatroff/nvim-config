a52ed1f64e68a00ae4459c4bddbf6e98e0aff814
Jasper Reef <mail@jasperreef.com>
2025-10-18 11:57:07 +0200

    migrate and refactor lsp.lua

--- lua/dax/lsp.lua
+++ lua/dax/lsp.lua
@@ -1,17 +1,19 @@
-local lsp_lines_enabled = falselsp
-local function toggle_lsp_lines()
-    lsp_lines_enabled = not lsp_lines_enabled
-    vim.diagnostic.config({
-        virtual_lines = lsp_lines_enabled,
-        virtual_text = not lsp_lines_enabled
-    })
-end
+local toggle_lsp_lines = (function()
+    local lsp_lines_enabled = true
+    return function()
+        lsp_lines_enabled = not lsp_lines_enabled
+        vim.diagnostic.config({
+            virtual_lines = lsp_lines_enabled,
+            virtual_text = not lsp_lines_enabled
+        })
+    end
+end)()
 toggle_lsp_lines()
 vim.keymap.set('n', '<C-x>', toggle_lsp_lines, { noremap = true })
 
 local cmp = require('cmp')
 
-vim.cmd("set completeopt=menu,menuone,noselect")
+vim.cmd('set completeopt=menu,menuone,noselect')
 local luasnip = require('luasnip')
 local cmp_kinds = {
   Text = '  ',
@@ -85,111 +87,85 @@ cmp.setup({
     }
 })
 
-local capabilities = require('cmp_nvim_lsp').default_capabilities(vim.lsp.protocol.make_client_capabilities())
-
-on_attach = require('dax.on_attach')
-
-local lsp = require('lspconfig')
-lsp.rust_analyzer.setup({
-    cmd = { "rust-analyzer" },
-    settings = {
-        ["rust-analyzer"] = {
-            procMacro = {
-                enable = true
-            },
-            checkOnSave = {
-                command = "clippy"
+local lsps = {
+    rust_analyzer = {
+        language = 'rust',
+        cmd = { 'rust-analyzer' },
+        settings = {
+            ['rust-analyzer'] = {
+                procMacro = {
+                    enable = true
+                },
             }
+        },
+    },
+    ts_ls = { language = 'typescript' },
+    purescriptls = {
+        language = 'purescript',
+        root_markers = { 'spago.yaml' },
+        settings = {
+            purescript = {
+                addSpagoSources = true,
+                censorWarnings = {
+                    'ShadowedName',
+                    'MissingTypeDeclaration'
+                }
+            },
+            formatter = 'purs-tidy'
+        },
+        flags = { debounce_text_changes = 150 },
+        more_setup = function()
+            vim.cmd("let g:neoformat_enabled_purescript = ['purstidy']")
+        end
+    },
+    hls = {
+        language = 'haskell',
+        filetypes = { 'haskell', 'cabal' },
+        settings = {
+            plugin = {
+                stan = {
+                    globalOn = false
+                }
+            },
+            haskell = { formattingProvider = 'fourmolu' },
         }
     },
-    on_attach = on_attach(),
-    capabilities = capabilities,
-})
-
-lsp.ts_ls.setup({
-    on_attach = on_attach('typescript'),
-    capabilities = capabilities,
-})
-
-vim.cmd("let g:neoformat_enabled_purescript = ['purstidy']")
-lsp.purescriptls.setup {
-    -- cmd = { "nc", "localhost", "3000" },
-    on_attach = on_attach('purescript'),
-    root_dir = lsp.util.root_pattern("spago.yaml"),
-    settings = {
-        purescript = {
-            addSpagoSources = true,
-            censorWarnings = {
-                "ShadowedName",
-                "MissingTypeDeclaration"
-            }
-        },
-        formatter = "purs-tidy"
+    eslint = { language = 'typescript' },
+    ocamllsp = { language = 'ocaml' },
+    wgsl_analyzer = {
+        language = 'wgsl',
+        more_setup = function()
+            vim.cmd('au BufNewFile,BufRead *.wgsl set filetype=wgsl')
+        end
     },
-    flags = {
-        debounce_text_changes = 150,
-    }
+    clangd = { language = 'c' },
+    pyright = { language = 'python' },
 }
 
-lsp.hls.setup {
-    on_attach = on_attach('haskell'),
-    filetypes = { 'haskell', 'cabal' },
-    settings = {
-        plugin = {
-            stan = {
-                globalOn = false
-            }
-        },
-        haskell = {
-            formattingProvider = "fourmolu",
-        },
-    }
-}
+local capabilities = require('cmp_nvim_lsp')
+    .default_capabilities(vim.lsp.protocol.make_client_capabilities())
+local on_attach = require('dax.on_attach')
 
-lsp.eslint.setup {
-    on_attach = on_attach('eslint')
-}
-
-lsp.ocamllsp.setup {
-    on_attach = on_attach('ocaml')
-}
+for name, config in pairs(lsps) do
+    config.capabilities = config.capabilities or capabilities
+    config.on_attach = config.on_attach or on_attach(config.language)
+    vim.lsp.config(name, config)
+    vim.lsp.enable(name)
+    if config.more_setup then config.more_setup() end
+end
 
 require('nvim-treesitter.configs').setup {
     ensure_installed = { 'c', 'rust', 'lua', 'tsx', 'python', 'typescript', 'haskell', 'scheme' },
     highlight = {
         enable = true,
-        -- disable = { 'tsx' }
     }
 }
 
-lsp.wgsl_analyzer.setup {
-    on_attach = on_attach('wgsl')
-}
-vim.cmd("au BufNewFile,BufRead *.wgsl set filetype=wgsl")
-
-lsp.clangd.setup {
-    on_attach = on_attach('c')
-}
-
-vim.tbl_deep_extend('keep', lsp, {
-    roc_language_server = {
-        filetypes = 'roc',
-        cmd = { "roc_language_server" },
-    }
-})
-
-require('lspconfig.configs').roc_language_server = {
-  default_config = {
-    cmd = { "roc_language_server" },
-    filetypes = { 'roc' },
-    root_dir = lsp.util.root_pattern("flake.nix"),
-    settings = {},
-  };
-}
-vim.cmd("au BufNewFile,BufRead *.roc set filetype=roc")
-lsp.roc_language_server.setup {
-    on_attach = on_attach('roc')
-}
-
-lsp.pyright.setup {}
+-- vim.lsp.config('purescript_analyzer', {
+--     cmd = { '/home/jasper/clones/purescript-analyzer/target/debug/purescript-analyzer' },
+--     filetypes = { 'purescript' },
+--     root_markers = { 'output', 'spago.lock' },
+--     on_attach = on_attach('purescript')
+-- })
+-- vim.lsp.enable('purescript_analyzer')
 
